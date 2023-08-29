a66f721e08177f49649a6d4035cb1152ca3eb7f3
Dennis Spek <dennisvdspek1@gmail.com>
2023-08-28 20:56:47 -0500

    Fixed type errors

--- src/middlewares/auth/auth.ts
+++ src/middlewares/auth/auth.ts
@@ -1,3 +1,7 @@
+import { Request, Response, NextFunction } from 'express';
+
+const apiKey = 'your-api-key';
+
 export function apiKeyMiddleware(req: Request, res: Response, next: NextFunction) {
   const providedApiKey = req.headers['api-key'] || req.query['api_key'];
   if (providedApiKey && providedApiKey === apiKey) {
